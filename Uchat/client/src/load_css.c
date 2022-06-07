38981c1599392edd3dcf4eee72d1382dff0949e4
osoldatko <soldatko97@gmail.com>
2022-06-07 16:48:18 +0300

    cat

--- Uchat/client/src/load_css.c
+++
@@ -1,7 +0,0 @@
-#include "../inc/header.h"
-
-void load_css(GtkCssProvider *provider, GtkWidget *widget) {
-    GtkStyleContext *context = gtk_widget_get_style_context(widget);
-    gtk_style_context_add_provider(context, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_USER); 
-}
-
